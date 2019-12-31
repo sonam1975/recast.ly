8b115f732ff05dc2d4d5b95277e22e0863018db7
sonam1975 <mingmar7@gmail.com>
2019-12-30 20:23:17 -0800

    Renders elements to the screen

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,23 @@
 //Import Elements
 //Add Elements to correct place
+import Search from './Search.js';
+import VideoPlayer from './VideoPlayer.js';
+import VideoList from './VideoList.js';
+
 
 var App = () => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>n
+        <div><h5><em>search</em><Search /></h5></div>
+      </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><em>videoPlayer</em> <VideoPlayer /></h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div><h5><em>videoList</em> <VideoList /></h5></div>
       </div>
     </div>
   </div>
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,4 +1,4 @@
-
+// import exampleVideoData from './data/exampleVideoData.js';
 var VideoList = () => (
   <div className="video-list">
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
