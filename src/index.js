cd2b62322d09922bd07658735cdc9e062e3ef4cc
sonam1975 <mingmar7@gmail.com>
2019-12-30 18:11:27 -0800

    Renders element to DOM

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 // TODO: Render the `App` component to the DOM
-import React from 'react';
-import ReactDOM from 'react';
 import App from './components/App.js';
 ReactDOM.render(<App />, document.getElementById('app'));
